4568224e9fb0eec571ec86060c7748596ab3936d
dabolink <dabolink@uvic.ca>
2015-02-22 17:58:05 -0800

    push fixed

--- server.py
+++ server.py
@@ -65,6 +65,7 @@ class MyService(rpyc.Service):
                     bottom_DHT[key] = rpyc.Service.DHT[key]
             rpyc.Service.DHT = bottom_DHT
             updateDHT(bottom_DHT)
+            print str(rpyc.Service.node_id) + " " + str(rpyc.Service.neighbour_id) + " " + str(rpyc.Service.DHT) + " " + str(rpyc.Service.neighbour_ip)
             return middle_id, 0, top_DHT, rpyc.Service.node_ip
         else:
             if rpyc.Service.neighbour_id > rpyc.Service.node_id:
