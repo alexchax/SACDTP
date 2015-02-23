847161f1c53e8e04f19c347370e4fa25d6ff2418
dabolink <dabolink@uvic.ca>
2015-02-22 18:14:55 -0800

    push fixed

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def putValue(key, value, conn):
 def main():
     conn = connect("10.19.1.11", 18861)
     while True:
-        for line in fileinput.input():
-            s = line.split(" ")
-            if s[0] == "get":
-                print getValue(int(s[1].strip()), conn)
-            elif s[0] == "put":
-                print putValue(int(s[1].strip()), int(s[2].strip()), conn)
+        line = raw_input()
+        s = line.split(" ")
+        if s[0] == "get":
+            print getValue(int(s[1].strip()), conn)
+        elif s[0] == "put":
+            print putValue(int(s[1].strip()), int(s[2].strip()), conn)
 
 if __name__ == "__main__":
     main()
