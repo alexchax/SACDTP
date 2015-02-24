7d3bd0c984056d6d0be1c8860d91b7439631c6d7
Alex Chachkov <alexchachkov@Alexs-MacBook-Pro.local>
2015-02-23 20:01:27 -0800

    logs

--- fabfile.py
+++ fabfile.py
@@ -22,7 +22,9 @@ env.hosts = [
     "slice330.pcvm1-1.instageni.wisc.edu",
   ]
 env.roledefs.update({
-    'server' : ["slice330.pcvm3-1.geni.case.edu", "slice330.pcvm3-1.instageni.metrodatacenter.com", "slice330.pcvm2-2.instageni.rnoc.gatech.edu"],
+    'the_special': ["slice330.pcvm3-1.geni.case.edu"],
+    'not_special': ["slice330.pcvm3-1.instageni.metrodatacenter.com"],
+    'not_special1': ["slice330.pcvm3-1.instageni.lsu.edu"],
 })
 
 
@@ -30,11 +32,40 @@ env.key_filename="./id_rsa"
 env.use_ssh_config = True
 env.ssh_config_path = './ssh-config'
 
-@roles('server')
 @parallel
-def setup_server():
+def update_servers():
     put('server.py')
-    run('apt-get install -y rpyc')
+    put('main.py')
+    run('rm DHT*')
+
+@parallel
+def setup_servers():
+    put('server.py')
+    put('main.py')
+    run('apt-get update')
+    run('apt-get install -y python-setuptools')
+    run('apt-get install -y python-pip')
+    run('sudo pip install rpyc')
+ 
+@roles('the_special')
+def run_server():
+    run('python server.py ')
+
+@roles('the_special')
+def the_special():
+    run('python main.py')
+    run('put 23 5')
+    run('put 1 4')
+    run('put 525 3')
+
+@roles('not_special')
+@parallel
+def run_servers():
+    run('python server.py 10.0.0.255')
+
+@roles('not_special1')
+def runservers():
+    run('python server.py 10.0.0.255')
 
 def uptime():
-    run('uptime')
+    run('ifconfig')
