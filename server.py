2ed71f521ac275c5f9c696982253885fbccb3d60
dabolink <dabolink@uvic.ca>
2015-02-23 18:38:03 -0800

    ummm what?

--- server.py
+++ server.py
@@ -167,7 +167,8 @@ class MyService(rpyc.Service):
                 conn = rpyc.connect(rpyc.Service.node.neighbour_ip, rpyc.Service.node.neighbour_port)
                 print "key: " + str(key) + " not found on server " + rpyc.Service.node.node_ip + " with ids: " + str(rpyc.Service.node.node_id) + " - " + str(rpyc.Service.node.neighbour_id)
                 print "passed to: " + rpyc.Service.node.neighbour_ip
-                return conn.root.put(key, value, rpyc.Service.node.node_ip)
+                tf = conn.root.put(key, value, rpyc.Service.node.node_ip)
+                return tf
             except socket.error:
                 print "connection error"
                 return False
