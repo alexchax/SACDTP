eb6a909af6469dd34ed4c9eb7bd24ae3b1263311
dabolink <dabolink@uvic.ca>
2015-02-23 21:53:21 -0800

    pushed fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 from rpyc import *
 import sys
 
@@ -17,15 +18,17 @@ def main():
                     print getValue(int(s[1].strip()), conn)
                 if s[0] == "put" and len(s) == 3:
                     print putValue(int(s[1].strip()),int(s[2].strip()), conn)
-    while True:
-        line = raw_input()
-        if line == "exit":
-            exit()
-        s = line.split(" ")
-        if s[0] == "get" and len(s) == 2:
-            print getValue(int(s[1].strip()), conn)
-        elif s[0] == "put" and len(s) == 3:
-            print putValue(int(s[1].strip()), int(s[2].strip()), conn)
+                sleep(1)
+    else:
+        while True:
+            line = raw_input()
+            if line == "exit":
+                exit()
+            s = line.split(" ")
+            if s[0] == "get" and len(s) == 2:
+                print getValue(int(s[1].strip()), conn)
+            elif s[0] == "put" and len(s) == 3:
+                print putValue(int(s[1].strip()), int(s[2].strip()), conn)
 
 if __name__ == "__main__":
     main()
