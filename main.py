6f404d41d8631184caba4bf4276cd38fb499097d
dabolink <dabolink@uvic.ca>
2015-02-23 21:47:21 -0800

    fixed main.py to accept files and error checking

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from rpyc import *
-import fileinput
+import sys
 
 def getValue(key, conn):
     return conn.root.get(key, "localhost")
@@ -9,12 +9,22 @@ def putValue(key, value, conn):
 
 def main():
     conn = connect("localhost", 18861)
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            for line in f:
+                s = line.split(" ")
+                if s[0] == "get" and len(s) == 2:
+                        print getValue(int(s[1].strip()), conn)
+                if s[0] == "put" and len(s) == 3:
+                    print putValue(int(s[1].strip()), conn)
     while True:
         line = raw_input()
+        if line == "exit":
+            exit()
         s = line.split(" ")
-        if s[0] == "get":
+        if s[0] == "get" and len(s) == 2:
             print getValue(int(s[1].strip()), conn)
-        elif s[0] == "put":
+        elif s[0] == "put" and len(s) == 3:
             print putValue(int(s[1].strip()), int(s[2].strip()), conn)
 
 if __name__ == "__main__":
